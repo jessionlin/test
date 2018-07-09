81be316aea88c97c6e782669a27e62448dcee798
linjiaxuan <1754317747@qq.com>
2018-07-09 19:34:41 +0800

    登录界面

--- client/pages/register/register.js
+++ client/pages/register/register.js
@@ -1,12 +1,6 @@
 
 Page({
   data: {
-
-  },
-  formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-  },
-  formReset: function () {
-    console.log('form发生了reset事件')
+    percent:25
   }
 })
