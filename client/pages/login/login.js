9e5b2c646745a2e8b1897df671ebedbe56d8e5c3
linjiaxuan <1754317747@qq.com>
2018-07-23 17:44:30 +0800

    前端

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -5,7 +5,9 @@ Page({
     
   },
   formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.navigateTo({
+      url: '/pages/index/index',
+    })
   },
   formReset: function () {
     console.log('form发生了reset事件')
